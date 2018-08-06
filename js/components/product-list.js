30e6d4b9860c3728200bba53feb2a3664f760236
Alex Rankin <alex.c.rankin@gmail.com>
2018-08-06 14:00:05 -0700

    set up item array

---
+++ js/components/product-list.js
@@ -0,0 +1,125 @@
+//import data from './data.js';
+
+//let items = data.items;
+
+// if(!items) {
+//     data.items = createItems();
+//     items = data.items;
+// }
+
+function createItems() {
+    let data = [{
+        id: 1,
+        name: 'bag',
+        image: '/images/bag.jpg',
+        viewed:
+        votes:
+    }, {
+        id: 2
+        name: 'banana',
+        image: '/images/banana.jpg',
+        viewed:
+        votes:
+    }, {
+        id: 3
+        name: 'bathroom',
+        image: '/images/bathroom.jpg',
+        viewed:
+        votes:
+    }, {
+        id: 4
+        name: 'boots',
+        image: '/images/boots.jpg',
+        viewed:
+        votes:
+    }   {
+        id: 5
+        name: 'breakfast',
+        image: '/images/breakfast.jpg',
+        viewed:
+        votes:
+    }    {
+        id: 6
+        name: 'bubblegum',
+        image: '/images/bubblegum.jpg',
+        viewed:
+        votes:
+    } {
+        id: 7
+        name: 'chair',
+        image: '/images/cthulhu.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 8
+        name: 'dog-duck',
+        image: '/images/dog-duck.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 9
+        name: 'dragon',
+        image: 'images/dragon.jpg'
+        viewed:
+        votes:  
+    }   {
+        id: 10
+        name: 'pen',
+        image: '/images/pen.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 11
+        name: 'pet-sweep',
+        image:'/images/pet-sweep.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 12
+        name: 'scissors',
+        image: '/images/scissors.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 13
+        name: 'shark',
+        image: '/images/shark.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 14
+        name: 'sweep',
+        image: '/images/sweep.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 15
+        name: 'tauntaun',
+        image: '/images/tauntaun.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 16
+        name: 'unicorn',
+        image: '/images/unicorn.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 17
+        name: 'usb',
+        image: '/images/usb.gif',
+        viewed:
+        votes:  
+    }   {
+        id: 18
+        name: 'water-can',
+        image: 'images/water-can.jpg',
+        viewed:
+        votes:  
+    }   {
+        id: 19
+        name: 'wine-glass'
+        image: 'images/wine-glass.jpg',
+        viewed:
+        votes:  
+    }]
