9363245be434915e8d9921e90604df2deeccf8d2
warrioryoko <miangpellegri@gmail.com>
2018-08-06 15:32:30 -0700

    3 static images displayed on page, side by side with nested borders.

--- js/components/app.js
+++ js/components/app.js
@@ -1,16 +1,25 @@
 // the template helper
 import html from '../html.js';
-// services component needs to use
+import images from '../data-wrangling/data.js';
+import imageLoading from '../data-wrangling/data.js';
 
-let template = function() {
+// services component needs to use
+let foo = imageLoading();
+
+let template = function(images) {
     return html`        
         <header>
             <h1>Greg's List</h1>
         </header>
         <main>
             <section class="auth"></section>
-            <section>
+            <section class="images">
                 <p><a href="for-sale.html">View For Sale Items</a></p>
+                <div class="row">
+                    <div class="column">${foo[0].src}</div>
+                    <div class="column">${foo[1].src}</div>
+                    <div class="column">${foo[2].src}</div>
+                </div>
             </section>
         </main>
     `;
@@ -19,6 +28,7 @@ let template = function() {
 export default class App {
 
     render() {
+        console.log(foo[0].src);
         let dom = template(); 
         this.authSection = dom.querySelector('section.auth');
         
---
+++ js/data-wrangling/data.js
@@ -0,0 +1,18 @@
+export default function imageLoading() {
+    let images = [
+        {
+            name: 'bag.jpg',
+            src: '<img src="./assets/bag.jpg" alt="It is a bag, what did you expect?" style="width:100%">'
+        },
+        {
+            name: 'banana.jpg',
+            src: '<img src="./assets/banana.jpg" alt="It\'s a bag, what did you expect?" style="width:100%">'
+        },
+        {
+            name: 'bathroom.jpg',
+            src: '<img src="./assets/bathroom.jpg" alt="It\'s a bathroom, whaddaya want?" style="width:100%">'
+        }
+    ];
+
+    return images;
+}
