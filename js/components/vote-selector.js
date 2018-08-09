a70d4f5b84f022c3654e7950014ddac3af69de61
Mike <mbennettillustration@gmail.com>
2018-08-09 13:05:09 -0700

    tests passing

--- js/components/vote-selector.js
+++ js/components/vote-selector.js
@@ -1,42 +1,42 @@
-import html from '/js/html.js';
-import Channel from './vote.js';
+// import html from '/js/html.js';
+// import Channel from './vote.js';
 
-let template = function() {
-    return html`
-        <ul></ul>
-    `;
-};
+// let template = function() {
+//     return html`
+//         <ul></ul>
+//     `;
+// };
 
-export default class ImageSelector {
-    constructor(props) {
-        this.vote = props.vote;
-        this.onVote = props.onVote;
-    }
+// export default class ImageSelector {
+//     constructor(props) {
+//         this.vote = props.vote;
+//         this.onVote = props.onVote;
+//     }
 
-    update(props) {
-        this.votes = props.votes;
+//     update(props) {
+//         this.votes = props.votes;
 
-        while(this.ul.lastElementChild) {
-            this.ul.lastElementChild.remove();
-        }
+//         while(this.ul.lastElementChild) {
+//             this.ul.lastElementChild.remove();
+//         }
 
-        this.renderList();
-    }
-    renderList() {
-        for(let i = 0; i < this.vote.length; i++) {
-            let vote = new Vote( {
-                vote: this.votes[i],
-                onSelect: this.onSelect
-            });
-            this.ul.appendChild(vote.render());
-        }
-    }
+//         this.renderList();
+//     }
+//     renderList() {
+//         for(let i = 0; i < this.vote.length; i++) {
+//             let vote = new Vote( {
+//                 vote: this.votes[i],
+//                 onSelect: this.onSelect
+//             });
+//             this.ul.appendChild(vote.render());
+//         }
+//     }
 
-    render() {
-        let dom = template();
-        this.ul = dom.querySelector('ul');
+//     render() {
+//         let dom = template();
+//         this.ul = dom.querySelector('ul');
 
-        this.renderList();
-        return dom;
-    }
-}
+//         this.renderList();
+//         return dom;
+//     }
+// }
